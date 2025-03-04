c3be93c8c0895080466b3c7592c3281c2b7bf237
Paras Samdhiya <samdhiyaparas21@gmail.com>
2025-03-04 14:57:59 +0530

    Add files via upload

---
+++ Decision Tree.py
@@ -0,0 +1,33 @@
+
+#Three lines to make our compiler able to draw:
+#import sys
+#import matplotlib
+
+
+import pandas
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+
+df = pandas.read_csv("data.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+X = df[features]
+y = df['Go']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+
+tree.plot_tree(dtree, feature_names=features)
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+#plt.savefig(sys.stdout.buffer)
+#sys.stdout.flush()
+
