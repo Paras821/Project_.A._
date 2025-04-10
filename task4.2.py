a15b7abb405acc1c7de3c04687830baaebd4e75b
Paras Samdhiya <samdhiyaparas21@gmail.com>
2025-04-10 15:29:56 +0530

    Add files via upload

---
+++ task4.2.py
@@ -0,0 +1,15 @@
+
+x = input("Enter a number")
+y = int(x)
+
+if y > 1:
+    for i in range(2,y):
+        if(y%i)==0:
+            print(y,"is a Composite number")
+            break
+    else:
+        print(y,"is a Prime number")
+
+else:
+    print(y,"is Neutral")
+
