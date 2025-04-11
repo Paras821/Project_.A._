40243ee384ad80cd8c69b7104daeafa69a99db16
Paras Samdhiya <samdhiyaparas21@gmail.com>
2025-04-11 14:41:03 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,31 @@
+
+for x in range(1900,2025):
+    if x % 4 == 0:
+        print(x)
+        print(" Leap Year ")
+    else:
+        print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = 1964
+b = 4
+print(a)
+if a % b == 0:
+    print(" Leap Year ")
+else:
+    print(" Not Leap Year ")
+
+
