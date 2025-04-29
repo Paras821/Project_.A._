9dfd8d919d4bcabc128b6b049528868b2c793c0d
Paras Samdhiya <samdhiyaparas21@gmail.com>
2025-04-29 15:14:39 +0530

    Add files via upload

---
+++ first joined in each dept.py
@@ -0,0 +1,16 @@
+
+import mysql.connector
+
+conn = mysql.connector.MySQLConnection(host="localhost", user="root", password="Root@123", database="mydatabase")
+
+mycursor = conn.cursor()
+
+sql = "SELECT e.id, e.name, e.dept, e.joined FROM employees e INNER JOIN (SELECT dept, MIN(joined) AS first_to_join FROM employees GROUP BY dept) first_joined ON e.dept = first_joined.dept AND e.joined =first_joined.first_to_join"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(f"Employee ID: {x[0]}, Name: {x[1]}, Dept: {x[2]}, Joined: {x[3]}")
+
